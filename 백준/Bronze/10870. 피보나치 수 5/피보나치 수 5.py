08752c25cb7b758a40b8011f96b15fe9bf24d2a0
Jaewoong Kim <73173265+jaewoooong@users.noreply.github.com>
2022-11-24 11:47:19 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,10 +1,19 @@
-def fibo(number:int)->int:
-    if number == 0:
+MAX_NUM = 20
+fibo_number = [0] * (MAX_NUM + 1)
+
+def is_calculated(number:int)->bool:
+    return number != 0
+
+def fibo(index:int)->int:
+    if index == 0:
         return 0
-    if number == 1:
+    if index == 1:
         return 1
-        
-    return fibo(number - 1) + fibo(number - 2)
+    if is_calculated(fibo_number[index]):
+        return fibo_number[index]
+    
+    fibo_number[index] = fibo(index - 1) + fibo(index - 2)
+    return fibo_number[index]
 
 def sol():
     ans = None
